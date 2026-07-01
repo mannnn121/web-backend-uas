602272a6f36301496b819e63b0573b46cef2d0ef
0b1111001 <anderusman2004@gmail.com>
2026-07-01 10:35:23 +0700

    testing

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,8 @@
+const RAILWAY_URL = "https://web-backend-uas-production.up.railway.app";
+
 const API_URL =
   typeof window !== "undefined" && !window.location.hostname.includes("localhost")
-    ? process.env.NEXT_PUBLIC_API_URL
+    ? process.env.NEXT_PUBLIC_API_URL || `${RAILWAY_URL}/api/backend`
     : "http://localhost:3000/api/backend";
 
 export default API_URL;
