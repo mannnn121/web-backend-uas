5b52bd8d10d253c549f66c7e4d1ab6a3e7bb01e8
0b1111001 <anderusman2004@gmail.com>
2026-07-01 10:27:54 +0700

    testing

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   typeof window !== "undefined" && !window.location.hostname.includes("localhost")
-    ? "/api/backend"
+    ? process.env.NEXT_PUBLIC_API_URL
     : "http://localhost:3000/api/backend";
 
 export default API_URL;
